56646ced72fdd3933cf5f9887c0ffb65663316fb
Sebastian Camacho <sxcamacho@gmail.com>
2015-04-16 22:15:36 -0300

    creamos migracion de usuarios con seeder y datos de prueba

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sebastian
+ * Date: 16/04/15
+ * Time: 08:25 PM
+ */
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Fake;
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+        $fake = Fake::create();
+        for($i = 0; $i < 30; $i++){
+            \DB::table('users')->insert(array(
+                'first_name' => $fake->firstName,
+                'last_name'  => $fake->lastName,
+                'uuid'       => \Uuid::generate(),
+                'email'      => $fake->unique()->email,
+                'password'   => Hash::make('secret'),
+                'type'       => $fake->randomElement($array = array ('user','admin','root'))
+            ));
+        }
+    }
+
+}
